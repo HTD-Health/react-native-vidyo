44b3694670a4b78eb8f162986bdffb23820831d3
jablonskipj <jablonski.pj@gmail.com>
2017-10-11 08:26:05 +0200

    preparation of new android library structure

---
+++ android/src/main/java/com/react/vidyo/VidyoPackage.java
@@ -0,0 +1,35 @@
+package com.react.vidyo;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by patrykjablonski on 11.10.2017.
+ */
+
+public class VidyoPackage implements ReactPackage {
+
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Arrays.<ViewManager>asList(
+                new VidyoComponent()
+        );
+    }
+}

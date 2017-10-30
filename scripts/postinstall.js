8cbca4bf25ea1562ca6447d8b8377db7b821f094
Tomasz Cieciński <tomasz.ciecinski@htdevelopers.com>
2017-10-30 09:14:36 +0100

    fixed path of FW source

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -2,7 +2,7 @@
 const extract = require('extract-zip')
 const pathAbsolute = require('path-absolute')
 
-extract('../VidyoClientIOS.framework.zip', {dir: pathAbsolute('../ios')}, function (err) {
+extract(pathAbsolute('../VidyoClientIOS.framework.zip'), {dir: pathAbsolute('../ios')}, function (err) {
   if(err) {
     throw err
   } 
